807872d46819842d73c64f74ca7af11c7230be47
quyentho <thokimquangquyen@gmail.com>
2023-02-20 23:30:05 +0700

    Setup project skeleton

---
+++ WesaamEcomerce.EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WesaamEcomerce.EntityFramework
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+
+}
---
+++ WesaamEcomerce.EntityFramework/DbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WesaamEcomerce.EntityFramework
+{
+    public class DbContextFactory: IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ApplicationDbContext> builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            var context = new ApplicationDbContext(
+                builder
+                .UseNpgsql("")
+                .UseSnakeCaseNamingConvention()
+                .Options);
+
+            return context;
+        }
+
+    }
+}
